ab786b60d323f62642bd745ed266cc862c53b9d1
WaelAlkeal <Wael.Alkeal@haw-hamburg.de>
2026-06-17 12:56:35 +0200

    Update!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 
 #include <iostream>
 
+#include "Vergleicher.h"
+#define ANZAHL_PROVIDER 50
+using namespace std;
 
 int main() {
-    printf("Hallo World\n");
+
+    Vergleicher v;
+
+    //IInternetProvider* dsl1 = new DSLProvider(); //bei new am ende Delete
+    //v.addProvider(dsl1);
+    //DSLProvider dsl;
+    //IInternetProvider* dsl2 = &dsl;
+    //v.addProvider(dsl2);
+
+    for (int i = 0; i< ANZAHL_PROVIDER ; ++i) {
+            v.addProvider();
+    }
+    v.compare();
+    cout<< "Anzahl Anbieter: "<< v.getAnzahlProvider()<< endl;
+    v.saveToFile();
     return 0;
 }
